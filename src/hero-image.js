56088287dd989f40ce32287cc8cfd4bcbe6c60ef
RicBKewL <ricbkewl@gmail.com>
2026-09-15 16:32:37 -0700

    Assemble ParFolio Mini hero image

---
+++ src/hero-image.js
@@ -0,0 +1,6 @@
+import a from './hero-chunk-1.js'
+import b from './hero-chunk-2.js'
+import c from './hero-chunk-3.js'
+import d from './hero-chunk-4.js'
+
+export default `data:image/webp;base64,${a}${b}${c}${d}`
